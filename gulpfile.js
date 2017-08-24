c05f0f5a17715dc478ee15065de056d25aa9954a
Clément Jacquet <clem.jacquet@gmail.com>
2017-08-24 17:49:43 +0900

    First gulpfile #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,29 @@
-/**
- * Created by Clement on 8/24/2017.
- */
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const git = require('gulp-git');
+
+gulp.task('launch', () => {
+	nodemon({
+		script: 'src/bin/www',
+		watch: ['src/'],
+		ext: 'js'
+	});
+});
+
+gulp.task('deploy', () => {
+	exec('git rebase master heroku', (err, stdout, stderr) => {
+		console.log(stdout);
+		console.error(stderr);
+		if (err) throw err;
+
+		git.push('heroku', 'master', (err) => {
+			if (err) throw err;
+
+			git.checkout('master', (err) => {
+				if (err) throw err;
+
+				console.log('Vocabulometer successfully deployed on Heroku!');
+			})
+		})
+	})
+});
